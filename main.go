3dff01c2e3f07601b416dcb0c7af27c7964e28e0
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-10-02 10:58:35 +0900

    Create templates

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.Run()
+}
